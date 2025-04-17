81470e0d75376e201c1682ea4662a7e4f1fe8cf7
Pengu1Nus <mustafin.zul@gmail.com>
2025-04-17 15:05:29 +0300

    feat: Add django custom command to check if db is avaliable. Add test for this.

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,30 @@
+"""
+Django команда для ожидания доступности БД.
+"""
+
+import time
+
+from django.core.management.base import BaseCommand
+from django.db.utils import OperationalError
+from psycopg2 import OperationalError as Psycopg2Error
+
+
+class Command(BaseCommand):
+    """Команда для ожидания БД."""
+
+    def handle(self, *args, **options):
+        self.stdout.write(self.style.WARNING('Ожидание Базы Данных...'))
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2Error, OperationalError):
+                self.stdout.write(
+                    self.style.ERROR(
+                        'База Данных недоступна, ожидание 1 сек...'
+                    )
+                )
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('База Данных доступна'))
