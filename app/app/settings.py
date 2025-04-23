bb8d6c75294bbbc44d090e9a2f67b90e6f60e718
Pengu1Nus <mustafin.zul@gmail.com>
2025-04-23 11:54:03 +0300

    fix: Add translation to models, model fields

--- app/app/settings.py
+++ app/app/settings.py
@@ -4,18 +4,15 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = (
     'django-insecure-f8stob5f1a!ae5!h5#=7rua21rv&v5ll)0^n+o11=w!&15p&ge'
 )
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
